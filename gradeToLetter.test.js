ba6f3635303520a4e0b5ff5172aed07ed8de7117
AmehFelix <52404663+AmehFelix@users.noreply.github.com>
2024-04-09 13:17:48 -0400

    Added 3 tests to grade testing

--- gradeToLetter.test.js
+++ gradeToLetter.test.js
@@ -17,4 +17,22 @@ describe("calculateGrade", () => {
     expect(gradeToLetter(65)).toBe("D");
   });
 
+  //New EPC
+  test("should return 'F' for a score < 60", () => {
+    expect(gradeToLetter(59)).toBe("F");
+  });
+
+  //New BVA
+  test(" lower boundary testing for inputs", () => {
+    expect(gradeToLetter(60)).toBe("D");
+    expect(gradeToLetter(70)).toBe("C");
+    expect(gradeToLetter(80)).toBe("B");
+    expect(gradeToLetter(90)).toBe("A");
+  });
+
+  //New EPC
+  test("should return error msg for an invalid input", () => {
+    expect(gradeToLetter('hello')).toBe("Invalid input. Please provide a number.");
+  });
+
 });
